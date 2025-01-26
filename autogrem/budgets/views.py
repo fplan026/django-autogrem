89c82a9f0244f84cfb5dc42c09e63df043f27ad2
Emmett Plancke <fplan026@gmail.com>
2025-01-26 14:57:06 -0400

    [FIX] link to homepage in navbar
    [REMOVE] budgets app; now obsolete (was for learning)

--- autogrem/budgets/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from .models import Budget
-
-def home(request):
-    budgets = Budget.objects.all()
-    return render(request, 'home.html', {'budgets': budgets})
